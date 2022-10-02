d480283c5cd1cb7cb684539b6aaf59d7047af7e8
romluc <romulofilhofilho@gmail.com>
2022-10-02 10:28:25 -0400

    Include example of requireActual

--- other/whats-a-mock/__tests__/thumb-war.4.js
+++ other/whats-a-mock/__tests__/thumb-war.4.js
@@ -2,7 +2,9 @@ import thumbWar from '../thumb-war'
 import * as utilsMock from '../utils'
 
 jest.mock('../utils', () => {
+  const actualUtils = require.requireActual('../utils') // Remember you can do this and overwrite only a subset of properties if you need the original ones
   return {
+    ...actualUtils,
     getWinner: jest.fn((p1, p2) => p2),
   }
 })
@@ -11,7 +13,7 @@ test('returns winner', () => {
   const winner = thumbWar('Ken Wheeler', 'Kent C. Dodds')
   expect(winner).toBe('Kent C. Dodds')
   expect(utilsMock.getWinner).toHaveBeenCalledTimes(2)
-  utilsMock.getWinner.mock.calls.forEach(args => {
+  utilsMock.getWinner.mock.calls.forEach((args) => {
     expect(args).toEqual(['Ken Wheeler', 'Kent C. Dodds'])
   })
 })
